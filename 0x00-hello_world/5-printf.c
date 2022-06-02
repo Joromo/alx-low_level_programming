fba3fed7281f3aff32a1f579d7a94810159df45e
Joromo <jefforomo@gmail.com>
2022-06-02 21:09:32 +0300

    .

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*Main : with proper grammar, but the outcome is a piece of art,
+*return zero else negative 
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
