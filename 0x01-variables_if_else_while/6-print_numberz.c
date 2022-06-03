b2c63a422c4e413a53b4e336f3e3eb1344a44345
Joromo <jefforomo@gmail.com>
2022-06-04 00:46:55 +0300

    .

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main-print
+* Return: 0 if code is executed correctly
+*/
+int main(void)
+{
+	int a;
+	/* */
+	a = 0;
+	/* */
+	while (a < 10)
+	{
+		putchar('0' + a);
+		a++;
+	}
+	return (0);
+}
