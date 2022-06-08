e950c96626e063b4c37a2636df88b40508ee4f20
Joromo <jefforomo@gmail.com>
2022-06-09 00:29:42 +0300

    .

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* add- add two intergers
+* @y: Interget to be added
+*@x:integer to be added
+* Return: result of addition
+*/
+int add(int y, int x)
+{
+	return (y + x);
+}
