c561b6fc7cc0a1134941e2a575245776cd53f2e9
Joromo <jefforomo@gmail.com>
2022-06-08 23:37:45 +0300

    .

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,24 +10,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 		_putchar('\n');
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 		_putchar('\n');
 	}
 	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 		_putchar('\n');
 	}
