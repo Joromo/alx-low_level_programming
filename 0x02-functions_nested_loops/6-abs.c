7a50df47192e22ce398dce5a056e37f3b9205d0d
Joromo <jefforomo@gmail.com>
2022-06-09 00:03:07 +0300

    .

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,11 +4,11 @@
 /**
 * _abs - computes the absolute value of an integer
 * @x: value to convert to an absolute value
-* Returns : 0 if code executed okay
+* Return: absolute value of x
 */
 int _abs(int x)
 {
-	if (x >= 0 || x <= 0)
+	if (x >= 0)
 	{
 		return (x);
 	}
