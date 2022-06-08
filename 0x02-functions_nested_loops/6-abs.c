f21b18103c5f2f0beb40709f035d8ec04d2f1381
Joromo <jefforomo@gmail.com>
2022-06-08 23:52:48 +0300

    .

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+* _abs- computes the absolute value of an integer
+* @x: value to convert to an absolute value
+* Returns : 0 if code executed okay
+*/
+int _abs(int x)
+{
+	if (x >= 0 || x <= 0)
+	{
+		_putchar (x);
+	}
+	return (0);
+}
