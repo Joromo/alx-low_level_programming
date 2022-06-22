4424f608ae13df918cc979c29c56f060e61986ba
Joromo <jefforomo@gmail.com>
2022-06-22 14:06:12 +0300

    .

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,11 +7,15 @@
 */
 int _sqrt_recursion(int n)
 {
+	if (n == 1)
+		return (1);
+	if (n == 0)
+		return (0);
 	if (n % 2 == 0)
 	{
 		return (_sqrt_recursion(n + 1));
 	}
 	if (n % 2 != 0)
 		return (-1);
-	return (0);
+	return (1);
 }
