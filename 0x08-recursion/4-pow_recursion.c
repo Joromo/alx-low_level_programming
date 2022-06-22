0efc2f5b699d6bd6315c7c6697e95d7af8ab2fa2
Joromo <jefforomo@gmail.com>
2022-06-22 13:45:38 +0300

    .

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _pow_recursion- return value of x raised to the power of y
+* @x: the base interger
+* @y: the Exponetial value
+* Return: the value of x ^ y or -1 if y > 0
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y <= 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
