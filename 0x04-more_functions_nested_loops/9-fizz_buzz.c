d40046583f2c13eae7c5376b5615d6d3e17dc254
Joromo <jefforomo@gmail.com>
2022-06-27 14:08:37 +0300

    .

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main- prints numbers 1 to 100. 3=FIZZ, 5-BUZZ bot fizzbuzz
+* Return: NUll
+*/
+int main(void)
+{
+	int i;
+	/* */
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" fizzbuzz ");
+		}
+		else
+		{
+			printf(" %d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
