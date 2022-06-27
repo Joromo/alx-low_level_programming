c5c3f1830e318f11f993180bedf222e226fe4192
Joromo <jefforomo@gmail.com>
2022-06-27 12:45:32 +0300

    .

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers- prints number 0-9
+* Return: Null if succesfull
+*/
+void print_numbers(void)
+{
+	int i = 0;
+	/* */
+	for (i = 0; i <= 9; 1++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
