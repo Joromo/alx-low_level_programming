0588b5e1486b77bf5eb80c749461abbeed96efb0
Joromo <jefforomo@gmail.com>
2022-06-30 22:40:32 +0300

    .

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,42 +14,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 	/* */
 	if (s1 == NULL)
-	{
 		a = 0;
-	}
 	else
-	{
 		for (a = 0; s1[a]; a++)
 		{
 		}
-	}
 	if (s2 == NULL)
-	{
 		b = 0;
-	}
 	else
-	{
 		for (b = 0; s2[b]; b++)
 		{
 		}
-	}
 	if (b > n)
-	{
 		b = n;
-	}
 	p = malloc(sizeof(char) * (a + b + 1));
 	if (p == NULL)
-	{
 		return (NULL);
-	}
 	for (c = 0; c < a; c++)
-	{
 		p[c] = s1[c];
-	}
 	for (c = 0; c < b; c++)
-	{
 		p[c + 1] = s2[c]
-	}
 	p[a + b] = '\0';
 	return (p);
 }
