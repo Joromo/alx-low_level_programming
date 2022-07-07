b54d6e45b481c912354d4dd99fa5f76a0ce1e04c
Joromo <jefforomo@gmail.com>
2022-07-07 14:24:22 +0300

    .

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,8 +20,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		num += va_arg(lt, int);
 		printf("%d", num);
 		if (i < n - 1 && separator)
-			printf("%d ", separator);
+			printf("%s ", separator);
 	}
 	printf("\n");
 	va_end(lt);
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_numbers(", ", 4, 0, 98, -1024, 402);
+    return (0);
 }
