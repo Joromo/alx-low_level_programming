bff0a79282fbc4b889d85b5831209599b8a93495
Joromo <jefforomo@gmail.com>
2022-07-07 23:31:03 +0300

    .

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,6 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list lt;
 	unsigned int i;
 	int num;
+	separator = ", ";
 	/* */
 	va_start(lt, n);
 	/* */
@@ -22,7 +23,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i < (n - 1) && separator)
 		{
 			printf("%s ", separator);
-			separator = ", ";
 		}
 	}
 	printf("\n");
